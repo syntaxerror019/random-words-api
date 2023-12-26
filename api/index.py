93c902dc38dde071ef2f48efb4fdf5f313d962c2
syntaxerror <142847239+syntaxerror019@users.noreply.github.com>
2023-12-26 17:35:29 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,16 +1,23 @@
-from flask import Flask, render_template, request, jsonify
-import random, json, os
+from flask import Flask, render_template, request, jsonify, g
+import random, json, os, time
 
 app = Flask(__name__)
+
+# Dictionary to store request timestamps and counts for each IP address
+request_data = {}
+
+# Maximum allowed requests per minute
+MAX_REQUESTS_PER_MINUTE = 30
+RATE_LIMIT_PERIOD = 60  # 60 seconds for a minute
+
+
 
 def find_words(json_file_path, num_words=1, num_letters=None): # Find words with the given criteria !.
     try:
         with open(json_file_path, 'r') as file:
             data = json.load(file)
-    except FileNotFoundError:
-        return json.dumps([{"word": "Error: File not found", "definitions": [], "synonyms": []}], indent=2, ensure_ascii=False)
     except json.JSONDecodeError:
-        return json.dumps([{"word": "Error: Invalid JSON format", "definitions": [], "synonyms": []}], indent=2, ensure_ascii=False)
+        return {"word": "Error: Invalid JSON format", "definitions": [], "synonyms": []}
 
     if num_words > len(data):
         num_words = len(data)
@@ -18,14 +25,16 @@ def find_words(json_file_path, num_words=1, num_letters=None): # Find words with
     selected_words = set()
      
     attempts = 0
+    
     max_attempts = num_words * 2
+    if num_letters is not None:
+        max_attempts *= 100
 
     while len(selected_words) < num_words and attempts < max_attempts:
         entry = random.choice(data)
         word = entry.get('word', '')
         if (num_letters is None or len(word) == num_letters) and word not in selected_words:
             selected_words.add(word)
-            #print("Adding word", word)
         attempts += 1
 
     result = []
@@ -47,6 +56,32 @@ def find_words(json_file_path, num_words=1, num_letters=None): # Find words with
     #return json.dumps(result, indent=2, ensure_ascii=False)
     return result
 
+@app.before_request
+def rate_limit():
+    # Get the IP address of the requester
+    ip_address = request.remote_addr
+
+    # Check if the IP address is in the request_data dictionary
+    if ip_address in request_data:
+        # Check if the time since the last request is within the rate limit period
+        elapsed_time = time.time() - request_data[ip_address]["timestamp"]
+
+        if elapsed_time < RATE_LIMIT_PERIOD:
+            # Increment the request count for the IP address
+            request_data[ip_address]["count"] += 1
+
+            # Check if the request count exceeds the limit
+            if request_data[ip_address]["count"] > MAX_REQUESTS_PER_MINUTE:
+                # Rate limit exceeded, return a 429 Too Many Requests response
+                return jsonify({"word": "Too many requests.", "definitions": "You are limited to 30 calls per minute in order to save resources on this serverless API, and let others enjoy low-latency. If you need a higher quota, reach out to me for your specific need, or deploy this API locally.", "synonyms": ["too fast","slow down"]}), 429
+        else:
+            # Reset the request count if the time period has passed
+            request_data[ip_address]["count"] = 1
+            request_data[ip_address]["timestamp"] = time.time()
+    else:
+        # If the IP is not present, initialize the count and timestamp
+        request_data[ip_address] = {"count": 1, "timestamp": time.time()}
+
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -57,13 +92,15 @@ def about():
     count = 1
     
     if request.args.get('length'):
-        length = request.args.get('length')
+        length = int(request.args.get('length'))
         
     if request.args.get('count'):
-        length = request.args.get('count')
-        
+        count = int(request.args.get('count'))
+    
+    print("count", count, "     length", length)
     current_directory = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(current_directory, "data.json")
     res = find_words(file_path, count, length)
     
     return jsonify(res)
+
