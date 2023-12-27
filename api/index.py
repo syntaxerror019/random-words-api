e0ec7d8e487d2c32d7baaaa4531891f4ed24a944
syntaxerror <142847239+syntaxerror019@users.noreply.github.com>
2023-12-27 08:50:17 -0500

    Update index.py
    skghdlkhdkfghdkh i touhced it aagin aaaaaaaaaaaaaaaa

--- api/index.py
+++ api/index.py
@@ -14,59 +14,6 @@ import random, json, os, time
 
 app = Flask(__name__)
 
-# Dictionary to store request timestamps and counts for each IP address
-request_data = {}
-
-# Maximum allowed requests per minute
-MAX_REQUESTS_PER_MINUTE = 30
-RATE_LIMIT_PERIOD = 60  # 60 seconds for a minute
-
-
-
-def find_words(json_file_path, num_words=1, num_letters=None): # Find words with the given criteria !.
-    try:
-        with open(json_file_path, 'r') as file:
-            data = json.load(file)
-    except json.JSONDecodeError:
-        return {"word": "Error: Invalid JSON format", "definitions": [], "synonyms": []}
-
-    if num_words > len(data):
-        num_words = len(data)
-        
-    selected_words = set()
-     
-    attempts = 0
-    
-    max_attempts = num_words * 2
-    if num_letters is not None:
-        max_attempts *= 100
-
-    while len(selected_words) < num_words and attempts < max_attempts:
-        entry = random.choice(data)
-        word = entry.get('word', '')
-        if (num_letters is None or len(word) == num_letters) and word not in selected_words:
-            selected_words.add(word)
-        attempts += 1
-
-    result = []
-
-    
-
-    if not selected_words:
-        result.append({"word": "Error: No matching words found", "definitions": [], "synonyms": []})
-    else:
-        for word in selected_words:
-            entry = next(entry for entry in data if entry.get('word') == word)
-            word_info = {}
-            word_info['word'] = entry.get('word', '')
-            word_info['definitions'] = entry.get('definitions', [])
-            word_info['synonyms'] = entry.get('synonyms', [])
-
-            result.append(word_info)
-
-    #return json.dumps(result, indent=2, ensure_ascii=False)
-    return result
-
 @app.before_request
 def rate_limit():
     # Get the IP address of the requester
@@ -98,20 +45,6 @@ def home():
     return render_template("index.html")
 
 @app.route('/random')
-def about():
-    length = None
-    count = 1
-    
-    if request.args.get('length'):
-        length = int(request.args.get('length'))
-        
-    if request.args.get('count'):
-        count = int(request.args.get('count'))
-    
-    print("count", count, "     length", length)
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(current_directory, "data.json")
-    res = find_words(file_path, count, length)
-    
-    return jsonify(res)
+def random():
+    return jsonify([{"word": "This API has moved!", "definitions": "random-word-api is no longer hosted on vercel. Either go to the main page of this vercel to get redirected to the new repl.it site, or use the following link in your project: https://random-word-api.sky-wired.repl.co/", "synonyms": ["404", "We Moved", "Get outa here!"]}])
 
